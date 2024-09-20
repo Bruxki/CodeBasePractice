aca41b0e69e456b051dce3c6edabad90f0c37c1c
Bruh <177808919+Bruxki@users.noreply.github.com>
2024-09-20 18:44:51 +0600

    Update RPG.cs

--- RPG.cs
+++ RPG.cs
@@ -12,17 +12,31 @@ public class Program
 		int playerHealth = 100;
 		int coins = 0;
 		
+		//Player location(fixed)
+		int playerX = 4;
+		int playerY = 2;
+		
 		while (gameOn)
 		{
 			MapRendering(map);
 			//Player health, coins tab:
 			Console.WriteLine($"Player Health: {playerHealth} \nCoins: {coins}");
 			Console.ReadLine();
+			
+			//Player movement
+			ConsoleKeyInfo key = Console.ReadKey();
+			if (key.Key == ConsoleKey.W && playerX > 0) playerX--;
+			else if (key.Key == ConsoleKey.S && playerX < 4) playerX++;
+			else if (key.Key == ConsoleKey.A && playerY > 0) playerY--;
+			else if (key.Key == ConsoleKey.D && playerY < 4) playerY++;
+			
+			map[playerX,playerY] = "P";
 		}
 	}
-	/*static void PlayerMovement(out int[,] playerPosition)
+	/*static void PlayerMovement(string[,] map)
 	{
-		Console.WriteLine("Make a move (wasd)");
+		
+		Console.ReadLine();
 		
 		
 		
@@ -48,5 +62,27 @@ public class Program
 			for (int j = 0; j < map.GetLength(1); j++)
 				map[i,j] = ".";
 		}
+		//Enemy location generation
+		Random rnd = new Random();
+		int x = rnd.Next(0,4);
+		int y = rnd.Next(0,4);
+		map[x,y] = "E";
+		
+		x = rnd.Next(0,4);
+		y = rnd.Next(0,4);
+		map[x,y] = "E";
+		//Treasure generation
+		x = rnd.Next(0,4);
+		y = rnd.Next(0,4);
+		//regenerate if it's the same
+		if (map[x,y] == "E")
+		{
+			x = rnd.Next(0,4);
+			y = rnd.Next(4,0);
+			map[x,y] = "T";
+		}
+		else
+		map[x,y] = "T";
+		
 	}
 }
