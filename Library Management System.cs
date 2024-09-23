6214c1854482f9cbc1dac877663c341b344c572a
Bruh <177808919+Bruxki@users.noreply.github.com>
2024-09-23 18:55:17 +0600

    Update Library Management System.cs

--- Library Management System.cs
+++ Library Management System.cs
@@ -43,20 +43,45 @@ public class Program
 		
 		Console.WriteLine("Welcome to the Library");
 		//menu
+	while (true)
+	{
 		string answer;
 		Menu(out answer);
+		//check all books
 		if (answer == "1")
-			FirstOption();
+		{
+			for (int i = 0; i < library.books.Length; i++)
+				{
+					Console.WriteLine("Title: " + library.books[i].title);
+					Console.WriteLine("Author: " + library.books[i].author);
+					Console.WriteLine("ISBN: " + library.books[i].isbn);
+					Console.WriteLine("Availability: " + library.books[i].isAvailable);
+				}
+		}
+		//check the availability
 		else if (answer == "2")
-			SecondOption();
-		
-		
+		{
+			Console.WriteLine("Please name the title of the book");
+			string name = Console.ReadLine().ToLower().Trim();
+			for (int i = 0; i < library.books.Length; i++)
+			{
+				if (library.books[i].title.Contains(name))
+				{
+					Console.WriteLine("Full Title: " + library.books[i].title + "\nAvailability: " + library.books[i].isAvailable);
+					break;
+				}
+				
+			}
+		}
 		//add a book
+		else if (answer == "5")
+			AddABook();
+		
 		
 			
 			
 			
-			
+	}		
 		void AddABook()
 		{
 			library.AddBook(newBook);
@@ -68,47 +93,20 @@ public class Program
 			Console.WriteLine("3) Take a book\n4) Return a book\n5) Add a newbook to the library");
 			Console.WriteLine("Please answer with a corresponding number");
 			while (true)
-			{
-				if (Console.ReadLine().ToLower().Trim() == "1")
-				{
-					answer = "1";
-					return;
-				}
-				else if (Console.ReadLine().ToLower().Trim() == "2")
-				{
-					answer = "2";
-					return;
-				}
-				else if (Console.ReadLine().ToLower().Trim() == "3")
-				{
-					answer = "2";
-					return;
-				}
-				else if (Console.ReadLine().ToLower().Trim() == "4")
-				{
-					answer = "2";
-					return;
-				}
-				else if (Console.ReadLine().ToLower().Trim() == "5")
-				{
-					answer = "2";
-					return;
-				}
-				else
-				{
-					Console.WriteLine("Please try again");
-					continue;
-				}
-			}
-		void FirstOption()
-		{
-			for (int i = 0; i < books.Length; i++)
-			{
-				Console.WriteLine("");
-			}
+            {
+                string input = Console.ReadLine().ToLower().Trim();
+                if (input == "1" || input == "2" || input == "3" || input == "4" || input == "5")
+                {
+                    answer = input;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Please try again");
+                }
+            }
 		}
-			
-		}
+	
 	}
 	
 }
