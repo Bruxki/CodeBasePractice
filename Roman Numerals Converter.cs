973d35a8e982d1d92ab3e9aa81a1c02ce0af5c13
Bruh <177808919+Bruxki@users.noreply.github.com>
2024-10-28 16:11:14 +0600

    Update Roman Numerals Converter.cs

--- Roman Numerals Converter.cs
+++ Roman Numerals Converter.cs
@@ -5,57 +5,53 @@ public class Program
 {
 	public static void Main()
 	{
-    //examples
+   		//examples
 		Console.WriteLine(ConvertFromRoman("MMMCMLXXV") );
 		Console.WriteLine(ConvertToRoman(3500) );
 	}
 	public static string ConvertToRoman(int num)
 	{
 		string final = "";
-		LookUp dictionary = new LookUp();
 		
-		for (int i = 0; i < dictionary.lookUpNumbers.Length; i++)
+		for (int i = 0; i < LookUp.lookUpNumbers.Length; i++)
 		{
-      //subtract the numbers and add corresponding roman letters >>
-			while (num - dictionary.lookUpNumbers[i] >= 0)
+			while (num - LookUp.lookUpNumbers[i] >= 0)
 			{
-				num -= dictionary.lookUpNumbers[i];
-				final += dictionary.lookUpLetters[i];
+				num -= LookUp.lookUpNumbers[i];
+				final += LookUp.lookUpLetters[i];
 			}
 		}
 		return final;
 	}
 	public static int ConvertFromRoman(string num)
 	{
-		LookUp dictionary = new LookUp();
 		int number = 0;
 		//going through all the letters in the roman number
 		for (int i = 0, k = 0; i < num.Length; i++, k++)
 			//going through all the letters in our library
-			for (int j = 0; j < dictionary.lookUpLetters.Length; j++)
+			for (int j = 0; j < LookUp.lookUpLetters.Length; j++)
 			{
 			//here we check if we have a 'double' roman number (IV, IX, etc.)
-			//since the last number can't be 'double' we check every 2-symbol-pairexcept the last letter
-			if (i < num.Length - 1 && num[i].ToString() + num[i+1].ToString() == dictionary.lookUpLetters[j])
+			//since the last number can't be 'double' we check every 2-symbol-pair but the last
+			if (i < num.Length - 1 && num[i].ToString() + num[i+1].ToString() == LookUp.lookUpLetters[j])
 			{
-				number += dictionary.lookUpNumbers[j];
+				number += LookUp.lookUpNumbers[j];
 				//skipping over 1 symbol
 				i++;
 				//if found break, so we don't count it twice
 				break;
 			}
-			else if (num[i].ToString() == dictionary.lookUpLetters[j])
-				number += dictionary.lookUpNumbers[j];
+			else if (num[i].ToString() == LookUp.lookUpLetters[j])
+				number += LookUp.lookUpNumbers[j];
 			}
 		
 		return number;
 	}
 	//Library with all the letters and corresponding values (same id)
-  //Though I could've done a dictionary (int, string)
-	class LookUp
+	static class LookUp
 	{
-		public string[] lookUpLetters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-		public int[] lookUpNumbers = {1000,   900,  500,  400, 100,  90,   50,  40,   10,  9,    5,    4,   1 };
+		public static string[] lookUpLetters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		public static int[] lookUpNumbers = {1000,   900,  500,  400, 100,  90,   50,  40,   10,  9,    5,    4,   1 };
 	}
 
 }
