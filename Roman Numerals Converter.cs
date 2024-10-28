d129aabb1b6602c123820949caf017cadf77fd8c
Bruh <177808919+Bruxki@users.noreply.github.com>
2024-10-28 14:59:21 +0600

    Create Roman Numerals Converter.cs

---
+++ Roman Numerals Converter.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+					
+public class Program
+{
+	public static void Main()
+	{
+    //examples
+		Console.WriteLine(ConvertFromRoman("MMMCMLXXV") );
+		Console.WriteLine(ConvertToRoman(3500) );
+	}
+	public static string ConvertToRoman(int num)
+	{
+		string final = "";
+		LookUp dictionary = new LookUp();
+		
+		for (int i = 0; i < dictionary.lookUpNumbers.Length; i++)
+		{
+      //subtract the numbers and add corresponding roman letters >>
+			while (num - dictionary.lookUpNumbers[i] >= 0)
+			{
+				num -= dictionary.lookUpNumbers[i];
+				final += dictionary.lookUpLetters[i];
+			}
+		}
+		return final;
+	}
+	public static int ConvertFromRoman(string num)
+	{
+		LookUp dictionary = new LookUp();
+		int number = 0;
+		//going through all the letters in the roman number
+		for (int i = 0, k = 0; i < num.Length; i++, k++)
+			//going through all the letters in our library
+			for (int j = 0; j < dictionary.lookUpLetters.Length; j++)
+			{
+			//here we check if we have a 'double' roman number (IV, IX, etc.)
+			//since the last number can't be 'double' we check every 2-symbol-pairexcept the last letter
+			if (i < num.Length - 1 && num[i].ToString() + num[i+1].ToString() == dictionary.lookUpLetters[j])
+			{
+				number += dictionary.lookUpNumbers[j];
+				//skipping over 1 symbol
+				i++;
+				//if found break, so we don't count it twice
+				break;
+			}
+			else if (num[i].ToString() == dictionary.lookUpLetters[j])
+				number += dictionary.lookUpNumbers[j];
+			}
+		
+		return number;
+	}
+	//Library with all the letters and corresponding values (same id)
+  //Though I could've done a dictionary (int, string)
+	class LookUp
+	{
+		public string[] lookUpLetters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		public int[] lookUpNumbers = {1000,   900,  500,  400, 100,  90,   50,  40,   10,  9,    5,    4,   1 };
+	}
+
+}
